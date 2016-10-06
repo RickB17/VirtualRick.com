ac459601339e2a6495f82b372db79f6c3b374618
Rick Breidenstein <RickB17@users.noreply.github.com>
2016-10-06 13:52:25 -0400

    Allowing the re-use of the client to leverage caching
    Allowing the re-use of the client to leverage caching. Reducing the time for each query after the initial one from almost 30 seconds to < 1 second.

--- Cisco/cisco-axl.py
+++ Cisco/cisco-axl.py
@@ -6,23 +6,26 @@ from suds.sax.element import Element
 import base64
 import ssl
 
-if hasattr(ssl, '_create_unverified_context'):
-        ssl._create_default_https_context = ssl._create_unverified_context
+def createClient():
+        cmserver = 'IP-ADDRESS'
+        cmport = '8443'
+        username = 'MyAXLuser'
+        password = 'MyPassword'
+        wsdl = 'file:///var/www/html/AXLAPI.wsdl'
+        location = 'https://' + cmserver + ':' + cmport + '/axl/'
 
-cmserver = 'IP-ADDRESS'
-cmport = '8443'
-wsdl = 'file:///var/www/html/AXLAPI.wsdl'
-location = 'https://' + cmserver + ':' + cmport + '/axl/'
-username = 'MyAXLUser'
-password = 'MyPassword'
-
-def getUser(userName):
+        if hasattr(ssl, '_create_unverified_context'):
+                ssl._create_default_https_context = ssl._create_unverified_context
         base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
         authenticationHeader = {
         "SOAPAction" : "ActionName",
         "Authorization" : "Basic %s" % base64string
         }
+
         client = Client(wsdl, location=location, username=username, password=password, headers=authenticationHeader)
+        return client
+
+def getUser(client, userName):
         userName = str(userName).lower()
         agentName = Element('userid').setText(userName)
         try:
@@ -32,23 +35,21 @@ def getUser(userName):
         except:
                 return "no device associated"
         
-def listUser(userName):
+def listUser(client, userName):
         eSearch = Element('searchCriteria')
         eUserid = Element('userid').setText('{0}'.format(userName))
         eSearch.append(eUserid)
         eReturn = Element('returnedTags')
         elastName = Element('lastName')
         eReturn.append(elastName)
-        client = Client(wsdl, location=location, username=username, password=password, headers=authenticationHeader)
-        result = client.service.listUser(eSearch,eReturn)
+        result = client.service.listUser(eSearch, eReturn)
         return result
 
-def getFields(ns):
-        #outputs the item to a text file so you can easily see what fields are available
-        client = Client(wsdl, location=location, username=username, password=password, headers=authenticationHeader)
+def getFields(client, ns):
         outputfile = open('{0}.txt'.format(ns),'w')
         OUTPUT = client.factory.create(ns)
         outputfile.write(str(OUTPUT))
         return '{0}.txt created'.format(ns)
 
-print getUser('Rick.Breidenstein')
+CLIENT = createClient()
+print getUser(CLIENT, 'Rick.Breidenstein')
